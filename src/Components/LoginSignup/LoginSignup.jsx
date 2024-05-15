9fff46a695d5a2f7ed2df293cf8f1b36808e1230
Niyathi Mariya George <nmg2881@gmail.com>
2024-05-16 00:05:50 +0530

    Added css

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -6,7 +6,7 @@ import person_icon from '../Assets/person.png'
 
 const LoginSignup = () => {
   return (
-    <div claasName='container'>
+    <div className='container'>
         <div className="header">
             <div className="text">SignUp</div>
             <div className="underline"></div>
@@ -14,19 +14,19 @@ const LoginSignup = () => {
         <div className="inputs">
             <div className="input">
                 <img src={person_icon}alt=""/>
-                <input type="text"/>
+                <input type="text" placeholder="Name"/>
             </div>
             <div className="input">
                 <img src={email_icon}alt=""/>
-                <input type="email"/>
+                <input type="email" placeholder="Email Id"/>
             </div>
             <div className="input">
                 <img src={password_icon}alt=""/>
-                <input type="password"/>
+                <input type="password" placeholder="Password"/>
             </div>
         </div>
       
-      <div className="forgot-password">Lost password<span>Click Here</span></div>
+      <div className="forgot-password">Lost password? <span>Click Here</span></div>
         <div className="submit-container">
             <div className="submit">SignUp</div>
             <div className="submit">LogIn</div>
